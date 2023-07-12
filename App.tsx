4119cb0c0d81b6240710328f4d79524a756a2280
Eng.Omar Bassam Shaheen <100515442+ENGOmarBassamShaheen@users.noreply.github.com>
2023-07-12 21:01:43 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Cards from './src/Screens/Cards';
+import Navigation from './src/Screens/Navigation';
+import Firestore1 from './src/database/firestore';
+const App = () => {
+  return <Cards />;
+};
+
+export default App;
